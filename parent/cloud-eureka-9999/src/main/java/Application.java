4cf1bbd450938d502517d5d43586543137a02e8e
AbsonChun <9882353+absonchun@user.noreply.gitee.com>
2023-04-25 16:28:52 +0800

    添加gataway模块

--- parent/cloud-eureka-9999/src/main/java/Application.java
+++
@@ -1,7 +0,0 @@
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-
-@EnableEurekaServer
-@SpringBootApplication
-public class Application {
-}
